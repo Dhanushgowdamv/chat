3d5437cb474f7f1a1610d2c74f44dfce043b56b2
Dhanushgowdamv <dhanushvenu199@gmail.com>
2025-03-06 00:00:59 +0530

    get all users api

--- backend/routs/user.routes.js
+++ backend/routs/user.routes.js
@@ -7,7 +7,7 @@ const router = Router();
 
 router.post(
   "/register",
-  body("name").isLength({min:3}).withMessage("hi mr/miss"),
+  body("name").isLength({ min: 3 }).withMessage("hi mr/miss"),
   body("email").isEmail().withMessage("email must be valid address"),
   body("password")
     .isLength({ min: 6 })
@@ -17,15 +17,23 @@ router.post(
 );
 
 // login
-router.post('/login',
-  body('email').isEmail().withMessage('Email must be a valid email address'),
-  body('password').isLength({ min: 3 }).withMessage('Password must be at least 3 characters long'),
-  userController.loginController);
+router.post(
+  "/login",
+  body("email").isEmail().withMessage("Email must be a valid email address"),
+  body("password")
+    .isLength({ min: 3 })
+    .withMessage("Password must be at least 3 characters long"),
+  userController.loginController
+);
 
-  router.get('/profile', authMiddleware.authUser, userController.profileController);
+router.get(
+  "/profile",
+  authMiddleware.authUser,
+  userController.profileController
+);
 
-  router.get('/logout' , authMiddleware.authUser, userController.logoutController);
+router.get("/logout", authMiddleware.authUser, userController.logoutController);
 
-
+router.get("/all", authMiddleware.authUser , userController.getAllUsersController);
 
 export default router;
--- backend/services/project.service.js
+++ backend/services/project.service.js
@@ -85,3 +85,6 @@ export const addUsersToProject = async ({ projectId, users, userId }) => {
   
     return updatedProject;
   };
+
+
+ 
